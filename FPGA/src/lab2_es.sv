c0bcbd88aa7ee1cbc84aa601b45bff4b3367d96a
ellieSundheim <57233862+ellieSundheim@users.noreply.github.com>
2024-09-08 16:15:38 -0700

    IT WORKS!
    added anode2_en and a shitload of testbench stuff

--- FPGA/src/lab2_es.sv
+++ FPGA/src/lab2_es.sv
@@ -6,7 +6,7 @@ Purpose: This file contains the modules needed to multiplex and run 2 seven sege
 
 module top(input logic [3:0] s1, s2,
             output logic [4:0] led,
-            output logic anode1_en,
+            output logic anode1_en, anode2_en,
             output logic [6:0] seg);
 
         // internal variables
@@ -17,7 +17,7 @@ module top(input logic [3:0] s1, s2,
          // structural verilog, modules go here
         oscillator myOsc(clk);
         led_logic myLEDLogic(s1, s2, led);
-        display_muxer myDisplayMuxer(clk, reset, s1, s2, anode1_en, sshow);
+        display_muxer #(16) myDisplayMuxer(clk, reset, s1, s2, anode1_en, anode2_en, sshow);
         seven_seg_disp mySevenSegDisp(sshow, seg);
 
 endmodule
@@ -36,15 +36,17 @@ module display_muxer #(parameter NUM_CYCLES_ON_EXP = 18) //NUM_CYCLES_ON_EXP set
                     (input logic clk,
 					 input logic reset,
                      input logic [3:0] s1,s2,
-                     output logic anode1_en,
+                     output logic anode1_en, anode2_en,
                      output logic [3:0] sshow);
 
     logic [NUM_CYCLES_ON_EXP-1:0] counter;
+
     always_ff @(posedge clk, posedge reset)
 		if (reset) counter <= 0;
-        else counter <= counter + 1;
+        	else counter <= counter + 1;
 
     assign anode1_en = counter[NUM_CYCLES_ON_EXP-1];
+	assign anode2_en = ~anode1_en;
     mux displayMux(anode1_en, s1, s2, sshow);
 
 endmodule
@@ -106,4 +108,54 @@ module seven_seg_disp(input logic[3:0] s,
 		seg = ~seg;
 	end 
 
+endmodule
+
+
+// testbench settings
+`timescale 1ns/1ns
+`default_nettype none
+`define N_TV 8
+
+//testbench
+module testbench();
+	logic clk, reset;
+	logic [3:0] s1, s2;
+	logic anode1_en, anode1_en_expected;
+	logic anode2_en, anode2_en_expected;
+	logic [4:0] led, led_expected;
+	logic [6:0] seg, seg_expected;
+	logic [3:0] sshow;
+	
+	
+	logic [31:0] vectornum, errors;
+	logic [10:0] testvectors[10000:0]; // vectors of format s1[3:0]_s2[3:0]_seg[6:0]
+	
+	//instantiate DUT
+	//top dut(s1, s2, led, anode1_en, seg);
+	oscillator myOsc(clk);
+	display_muxer #(18) myDispMux (clk, reset, s1, s2, anode1_en, anode2_en, sshow);
+	
+	
+	//generate clock signal
+/*
+	always 
+		begin
+			clk = 1; #5;
+			clk = 0; #5;
+		end*/
+		
+		
+	// At the start of the simulation:
+	//  - Load the testvectors
+	//  - Pulse the reset line (if applicable)
+ initial
+   begin
+     // $readmemb("lab2\sim\led_logic_testvectors.tv", testvectors, 0, `N_TV - 1);
+     vectornum = 0; errors = 0;
+     reset = 0; #12; reset = 1; #27; reset = 0;
+	 
+     s1 = 4'b0001; s2 = 4'b0010; #10;
+	 
+   end
+  
 endmodule
